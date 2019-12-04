c7e89499a79f8d0e5dd6fd7015e3e790a8e8ede5
Raylison Nunes dos Santos <raylison100@gmail.com>
2019-12-04 00:08:30 -0300

    Update

---
+++ Assets/Scripts/LiberaTotem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LiberaTotem : MonoBehaviour
+{
+   
+    public bool liberaPainel;
+    public float time;
+    public GameObject painel;
+    public Text contador;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        liberaPainel = false;
+        time = 15;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        painel.SetActive(liberaPainel);
+
+        if (liberaPainel && time > 0 )
+        {
+            time -= Time.deltaTime;
+            contador.text = time.ToString();
+        }
+        else
+        {
+            liberaPainel = false;
+        }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("totem"))
+        {
+            liberaPainel = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+}
